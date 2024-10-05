dcb1d4e260d5722115ab794f4be3fac1d7a0e308
Sema Yasharova <semayasharova@gmail.com>
2024-10-05 09:36:06 +0300

    ParkSmart Updated Parking Lot

---
+++ js/carsShow.js
@@ -0,0 +1,37 @@
+function toggleParkingSpot(event) {
+    const gridItem = event.target.closest('.grid-item'); 
+    
+    const carImages = [
+        './images/car1.png',
+        './images/car4.png',
+        './images/car5.png'
+    ];
+
+    if (gridItem.classList.contains('occupied')) {
+        gridItem.classList.remove('occupied');
+        gridItem.innerHTML = gridItem.id; 
+    } else {
+        gridItem.classList.add('occupied');
+        gridItem.innerHTML = ''; 
+
+        const randomIndex = Math.floor(Math.random() * carImages.length);
+        const randomCarImage = carImages[randomIndex];
+
+        const img = document.createElement('img');
+        img.src = randomCarImage;
+        img.alt = 'Car';
+        img.style.width = '80%';
+        img.style.height = '50%';
+        img.style.objectFit = 'contain';
+        img.style.borderRadius = '5px'; 
+
+        gridItem.appendChild(img);
+    }
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    const parkingSpots = document.querySelectorAll('.grid-item');
+    parkingSpots.forEach(spot => {
+        spot.addEventListener('click', toggleParkingSpot);
+    });
+});
